051d33a96aec02e532cac8a500df2e1b8fa9f427
Raunak Poonia <63884478+aabbcc333@users.noreply.github.com>
2026-04-01 07:11:52 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.go
@@ -0,0 +1,15 @@
+func dailyTemperatures(temperatures []int) []int {
+n := len(temperatures)
+result := make([]int,n)
+stack := []int{}
+
+for i:=0;i<n;i++{
+	for len(stack) > 0 && temperatures[i]>temperatures[stack[len(stack)-1]]{
+		prev := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		result[prev] = i-prev
+	}
+	stack = append(stack, i)
+}
+return result 
+}
